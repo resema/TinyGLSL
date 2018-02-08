5fd9146856e8dcc4ae2de0a8ebc3945999005229
resema <5762933+resema@users.noreply.github.com>
2018-02-08 06:32:19 +0100

    loading shader partially implemented

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,7 @@ using namespace glm;
 int main( void )
 {
 	// Initialise GLFW
-	if( !glfwInit() )
-	{
+	if (!glfwInit()) {
 		fprintf( stderr, "Failed to initialize GLFW\n" );
 		getchar();
 		return -1;
@@ -33,7 +32,7 @@ int main( void )
 
     // Open a window and create its OpenGL context
 	window = glfwCreateWindow( 1024, 768, "Tutorial 02 - Red triangle", NULL, NULL);
-	if( window == NULL ){
+	if (window == NULL) {
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
 		getchar();
 		glfwTerminate();
