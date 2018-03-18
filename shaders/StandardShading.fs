fe01347e1a93084ce962de083e3bc35a17d4bad2
resema <5762933+resema@users.noreply.github.com>
2018-03-18 16:05:37 +0100

    basic light shading

--- shaders/StandardShading.fs
+++ shaders/StandardShading.fs
@@ -28,4 +28,37 @@ void main()
 
     // distance to the light
     float distance = length(LightPosition_worldspace - Position_worldspace);
+
+    // normal of the computed fragment, in camera space
+    vec3 n = normalize(Normal_cameraspace);
+
+    // direction of the light (from the fragment to the light)
+    vec3 l = normalize(LightDirection_cameraspace);
+
+    // cosine of the angle between the normal and the light direction, clamped above 0
+    //  - light is at the vertical of the triangle -> 1
+    //  - light is perpendicalur to the triangle -> 0
+    //  - light is behind the triangle -> 0
+    float cosTheta = clamp(dot(n,l), 0, 1);     // clamp between 0 and 1
+
+    // eye vector (towards the camera)
+    vec3 E = normalize(EyeDirection_cameraspace);
+
+    //direction in which the triangle reflects the light
+    vec3 R = reflect(-l, n);
+
+    // cosing of the angle between the Eye and the Reflect vector
+    //  - looking into the reflection -> 1
+    //  - looking elsewhere -> (< 1)
+    float cosAlpha = clamp(dot(E,R), 0, 1);
+
+    // final color
+    color =
+            // ambient : simulates indirect lighting 
+            MaterialAmbientColor +
+            // diffuse : "color" of the object
+            MaterialDiffuseColor * LightColor * LightPower * cosTheta / pow(distance,2) +
+            // specular : reflective highlight, like a mirror
+            MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha, 5) / pow(distance,2);
+
 }
